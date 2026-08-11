44bb82351087f76eeb1b448cdf2ce5b97dc084f8
journewki <88959869+journewki@users.noreply.github.com>
2026-08-11 22:01:57 +0900

    [Passed] 달에서 무게 구하기 (2ms, 0MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float a= 13;
+    float b = 0.165;
+    cout<<a<<" * "<<fixed<<setprecision(6)<<b<<" = "<<fixed<<setprecision(6)<<a*b;
+    
+    // Please write your code here.
+    return 0;
+}
