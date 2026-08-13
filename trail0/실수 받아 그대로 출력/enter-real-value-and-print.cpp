293bd8033021b0fc48ef8adebff6e6153d145a36
journewki <88959869+journewki@users.noreply.github.com>
2026-08-13 18:52:57 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail0/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float n;
+    cin>>n;
+    cout<<fixed<<setprecision(2)<<n;
+    return 0;
+}
