12e7397e6619a348fb735a4d142cc01268f2401b
journewki <88959869+journewki@users.noreply.github.com>
2026-08-13 19:01:42 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a,b,c;
+    cin>>a>>b>>c;
+    cout<<fixed<<setprecision(3)<<a<<"\n"<<b<<"\n"<<c;
+
+    return 0;
+}
