2836a8384a88251c356f0a344065ec13ef63d165
journewki <88959869+journewki@users.noreply.github.com>
2026-08-14 11:36:56 +0900

    [Passed] 합과 평균의 차 (2ms, 0MB)

---
+++ trail0/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c<<"\n"<<(a+b+c)/3<<"\n"<<2*(a+b+c)/3;
+
+    return 0;
+}
