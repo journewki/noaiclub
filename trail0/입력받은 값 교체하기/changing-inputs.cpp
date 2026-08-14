662bcff604118e46f3e07a7e2b16d3797cd5b743
journewki <88959869+journewki@users.noreply.github.com>
2026-08-14 11:25:34 +0900

    [Passed] 입력받은 값 교체하기 (2ms, 0MB)

---
+++ trail0/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    cout<<b<<" "<<a;
+    return 0;
+}
